9b9afe74e612ef432a04e5aec9bdcd106a5d1ae6
IngaKirsona <ingas@inbox.lv>
2020-09-12 19:55:41 +0300

    Initial Commit

---
+++ WebRecipes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WebRecipes
+//
+//  Created by Inga Kirsona on 12/09/2020.
+//  Copyright © 2020 Inga Kirsona. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
