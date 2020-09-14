e288a4b3834f2f58ff264d320dd1ade60265cd42
IngaKirsona <ingas@inbox.lv>
2020-09-14 15:56:48 +0300

    hometask!

--- WebRecipes/ListTableViewController.swift
+++ WebRecipes/ListTableViewController.swift
@@ -7,83 +7,108 @@
 //
 
 import UIKit
+import CoreData
 
 class ListTableViewController: UITableViewController {
-
-    var recipesList = [
-        "https://www.bbcgoodfood.com/recipes/vegan-chickpea-curry-jacket-potato",
-        "https://www.bbcgoodfood.com/recipes/satay-sweet-potato-curry",
-        "https://www.bbcgoodfood.com/recipes/mint-basil-griddled-peach-salad"
-    ]
+    var recipes = [Recipe]()
     
+    var context: NSManagedObjectContext?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        let appDeleggate = UIApplication.shared.delegate as! AppDelegate
+        context = appDeleggate.persistentContainer.viewContext
+        loadData()
     }
-
-  
-
-
-//-------> count the rows
+    
+    @IBAction func addButtonTapped(_ sender: Any) {
+        addNewRecipe()
+    }
+    
+func addNewRecipe(){
+        let alertController = UIAlertController(title: "Recipe!", message: "Do you want to add a new recipe?", preferredStyle: .alert)
+        alertController.addTextField { (textField: UITextField) in
+            //-------> what will be seen fairly in add cell, when nothing is typed
+            textField.placeholder = "Enter the web page of your recipe"
+        }
+        //-------> to add webpage
+        let addAction = UIAlertAction(title: "Add", style: .cancel) { (action: UIAlertAction) in
+            let textField = alertController.textFields?.first
+            let entity = NSEntityDescription.entity(forEntityName: "Recipe", in: self.context!)
+            let recipe = NSManagedObject(entity: entity!, insertInto: self.context)
+            //-------> to add text to item
+            recipe.setValue(textField?.text, forKey: "webPage")
+            //-------> to save text to Recipe
+            self.saveData()
+            //-------> need to load because it reloads tableview
+            self.loadData()
+        }//end addAction
+        //-------> to add cancel
+        let cancelAction = UIAlertAction(title: "Cancel", style: .default, handler: nil)
+        
+        alertController.addAction(addAction)
+        alertController.addAction(cancelAction)
+        present(alertController, animated: true)
+    }
+    
+    //-------> to fetch and reload a tableview
+    func loadData(){
+        let request: NSFetchRequest<Recipe> = Recipe.fetchRequest()
+        do{
+            let result = try context?.fetch(request)
+            recipes = result!
+        }catch{
+            fatalError(error.localizedDescription)
+        }
+        tableView.reloadData()
+    }
+    
+    func saveData(){
+        do {
+            try self.context?.save()
+        }catch{
+            fatalError(error.localizedDescription)
+        }
+        loadData()
+    }
+    
+    //-------> count the rows
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return recipesList.count
+        return recipes.count
     }
-
-
+    //-------> cell formatting
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "webCell", for: indexPath)
-
-        cell.textLabel?.text = recipesList[indexPath.row]
-//-------> to allow rows as many needed
+        let recipe = recipes[indexPath.row]
+        cell.textLabel?.text = recipe.value(forKey: "webPage") as? String
+        cell.selectionStyle = .none
+        // cell.textLabel?.text = recipesList[indexPath.row]
+        //-------> to allow rows as many needed
         cell.textLabel?.numberOfLines = 0
-         return cell
+        return cell
     }
-//-------> when clicked on row, navigate to webViewController
+    
+    //-------> when clicked on row, navigate to webViewController
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
-//-------> to access vebWieController and asign passedValue to recipiesList
+        //-------> to access vebWieController and asign passedValue to recipiesList
         let vc: WebViewController = storyBoard.instantiateViewController(withIdentifier: "WebViewController") as! WebViewController
-        vc.passedValue = recipesList[indexPath.row]
+        vc.passedValue = recipes[indexPath.row].webPage!
         self.present(vc, animated: true)
     }
-  
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
     
-    // Override to support editing the table view.
+    //-------> to add delete option and alert
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+            let alert = UIAlertController(title: "Are you sure you want to delete?", message: nil, preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+            alert.addAction(UIAlertAction(title: "Delete", style: .default, handler: { (_) in
+                self.context?.delete(self.recipes[indexPath.row])
+                UIView.transition(with: tableView, duration: 1.0, options: .transitionCurlUp, animations: {
+                    self.loadData()
+                    self.saveData()
+                }, completion: nil)
+            }))
+            self.present(alert, animated: true)}
     }
-    
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    
-
 }
